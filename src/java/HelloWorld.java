37a422ccf2a11ee23a19d3507ca48f33fd8d25d1
21HP1A1239 . <lokeshmatha2004@gmail.com>
2025-04-18 18:10:03 +0530

    initial commit

---
+++ src/java/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.example;
+
+import java.util.*;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins + Maven!");
+    }
+}
